7ad52b8bb944ba4e845eee885a6a147346d5fda4
Bobelkop <Rkraru25@student.aau.dkgit config --global user.email Rkraru25@student.aau.dk>
2025-11-13 17:41:42 +0100

    Update platformio.ini and main.cpp to include Keypad library and streamline setup

--- Audrino Keypad/src/main.cpp
+++ Audrino Keypad/src/main.cpp
@@ -1,18 +1,12 @@
 #include <Arduino.h>
+#include <Keypad.h>
 
-// put function declarations here:
-int myFunction(int, int);
 
 void setup() {
-  // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+ Keypad_setup() ;
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+ Keypad_loop() ;
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
